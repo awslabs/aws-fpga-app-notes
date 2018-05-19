f1a1a1b6486711d4de4727912bb703bcc4893cf8
ThomasXilinx <tbollaer@xilinx.com>
2018-05-18 18:18:39 -0700

    Update get_ffmpeg.sh

--- reInvent17_Developer_Workshop/ffmpeg/build/get_ffmpeg.sh
+++ reInvent17_Developer_Workshop/ffmpeg/build/get_ffmpeg.sh
@@ -1,6 +1,6 @@
 H=$PWD/ffmpeg_hevc
 
-echo ======================== BUILDING ffmpeg =========================
+echo ======================== GETTING ffmpeg =========================
 cd $H/sources
 rm -rf ffmpeg
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg -b release/3.3
